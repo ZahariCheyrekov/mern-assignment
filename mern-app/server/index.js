4c345427d00e539004c3b0dd4911113404fbd097
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-13 22:01:30 +0300

    Format the code

--- mern-app/server/index.js
+++ mern-app/server/index.js
@@ -1,16 +1,16 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-  res.send("Application is running correctly.");
-});
+app.get('/', (req, res) => {
+  res.send('Application is running correctly.')
+})
 
-app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
