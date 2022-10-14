8d45224662587c3a88d72cfa35c7928c1a23e22e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-14 10:19:45 +0300

    Configure database

--- mern-app/server/index.js
+++ mern-app/server/index.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+import { configDatabase } from './config/configDatabase.js'
+import { DEFAULT_PORT } from './constants/appConstants.js'
 
 const app = express()
+dotenv.config()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || DEFAULT_PORT
 
 app.get('/', (req, res) => {
   res.send('Application is running correctly.')
 })
 
-app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
+configDatabase(app, PORT)
